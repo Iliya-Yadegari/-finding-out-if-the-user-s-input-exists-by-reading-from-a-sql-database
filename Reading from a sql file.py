9d2321f75cdc981e24269c2ad350d846d466b8ef
Iliya-Yadegari <naruto3@email.com>
2021-05-06 20:41:50 +0100

    Update Reading from a sql file.py

--- Reading from a sql file.py
+++ Reading from a sql file.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import ReadingFromSqlModule as rfsm
 
 window = Tk()
 window.title('Reading from a sql database')
@@ -8,6 +9,6 @@ r = IntVar()
 Radiobutton(window,text = 'Read',variable = r,value = 1).grid(row = 0, column = 0, padx = 10, pady = 10)
 Radiobutton(window,text = 'Write',variable = r,value = 2).grid(row = 1, column = 0, padx = 10, pady = 10)
 
-submit_btn = Button(window,text = 'Submit',width = 20,height = 3).grid(row = 2, column = 0, padx = 10, pady = 10)
+submit_btn = Button(window,text = 'Submit',width = 20,height = 3,command = lambda: rfsm.res_fun(r.get(),window)).grid(row = 2, column = 0, padx = 10, pady = 10)
 
 window.mainloop()
