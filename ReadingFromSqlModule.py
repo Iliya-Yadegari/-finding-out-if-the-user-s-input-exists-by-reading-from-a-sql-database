989cb945655822e8a30a4d0d0e83a40774bc75ab
Iliya-Yadegari <naruto3@email.com>
2021-05-07 18:26:30 +0100

    Update ReadingFromSqlModule.py

--- ReadingFromSqlModule.py
+++ ReadingFromSqlModule.py
@@ -24,6 +24,8 @@ def res_fun(value,w):
         
         writeSubmit_btn.grid(row = 3, column = 0, padx = 10, pady = 10)
         
+       
+        
     elif value == 1:
         read_w = Toplevel(w)
         nameRead_lbl = Label(read_w,text = 'Enter the name you want to search for ===>').grid(row = 0, column = 0, padx = 10, pady = 10)
@@ -35,26 +37,22 @@ def res_fun(value,w):
         ageRead_lbl = Label(read_w,text = 'Enter the age you want to search for ===>').grid(row = 2, column = 0, padx = 10, pady = 10)
         global ageRead_entry
         ageRead_entry = Entry(read_w)
-        
-        readSubmit_btn = Button(read_w,text = 'Submit',width = 20,height = 3,command = lambda: read_fun(nameRead_entry.get())).grid(row = 3, column = 0, padx = 10, pady = 10)
+
+        readSubmit_btn = Button(read_w,text = 'Submit',width = 20,height = 3,command = lambda: read_fun(nameRead_entry.get(),lastNameRead_entry.get(),ageRead_entry.get())).grid(row = 3, column = 0, padx = 10, pady = 10)
         
         nameRead_entry.grid(row = 0, column = 1, padx = 10, pady = 10)
         lastNameRead_entry.grid(row = 1, column = 1, padx = 10, pady = 10)
         ageRead_entry.grid(row = 2, column = 1, padx = 10, pady = 10)
 
-    
-mydb = msc.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '',
-    database = 'readingfromsql'
-    )
+        name_e = nameRead_entry.get()
+        lastName_e = lastNameRead_entry.get()
+        age_e = ageRead_entry.get()
+        
 
-mycursor = mydb.cursor()
 
 def write_fun(lastName):
     
-    if name_entry.get() == '' or lastName_entry.get() == '' or age_entry.get() == '':
+    if name_entry.get() == '' or lastName == '' or age_entry.get() == '':
         
         messagebox.showerror('Error','You have left one or more of the areas blank')
         
@@ -65,16 +63,27 @@ def write_fun(lastName):
         mycursor.execute(sqlCommand,values)
         mydb.commit()
         
-def read_fun(nameRead):
+def read_fun(name_e,lastName_e,age_e):
     mycursor.execute('SELECT * FROM writtenres')
 
     result = mycursor.fetchall()
-    
-    if nameRead == '' or lastNameRead_entry.get() == '' or ageRead_entry.get() == '':
+    name_e += lastName_e
+    name_e += age_e    
+    if name_e == '':
+        print(all_data)
         messagebox.showerror('Error','You have left one or more of the areas blank')
         
-    elif result[0] == nameRead and result[1] == lastNameRead_entry.get() and result[2] == ageRead_entry.get():
+    elif ''.join(result[0]) == name_e:
         messagebox.showinfo('Result','This row exists')
     
     else:
         messagebox.showinfo('Result','This row does not exist')
+    
+mydb = msc.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'readingfromsql'
+    )
+
+mycursor = mydb.cursor()
